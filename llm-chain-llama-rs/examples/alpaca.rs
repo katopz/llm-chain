d7b97623e2b567451278a851d5a2f0f884a14fc5
katopz <katopz@gmail.com>
2023-04-13 10:37:51 +0700

    formatted

--- llm-chain-llama-rs/examples/alpaca.rs
+++ llm-chain-llama-rs/examples/alpaca.rs
@@ -31,31 +31,29 @@ fn main() {
 #[tokio::test]
 async fn hello() {
     let _infer = Infer {
-        model_load:ModelLoad{
-            model_path: "/Users/katopz/git/katopz/llm-chain/llm-chain-llama-rs/examples/ggml-alpaca-7b-q4.bin".to_string(),
+        model_load: ModelLoad {
+            model_path: "/Users/katopz/git/katopz/ggml-alpaca-7b-q4.bin".to_string(),
             num_ctx_tokens: 2048usize,
         },
-        prompt_file: PromptFile {
-            prompt_file: None,
-        },
+        prompt_file: PromptFile { prompt_file: None },
         generate: Generate {
             num_threads: None,
             num_predict: None,
             batch_size: 8usize,
-            repeat_last_n: 64usize ,
-            repeat_penalty : 1.3f32,
+            repeat_last_n: 64usize,
+            repeat_penalty: 1.3f32,
             temperature: 0.1f32,
-            top_k :40usize,
+            top_k: 40usize,
             top_p: 0.95f32,
             load_session: None,
             seed: None,
             float16: false,
             token_bias: None,
-            ignore_eos: false,   
+            ignore_eos: false,
         },
         prompt: Some("Say 1 to 3.".to_string()),
-        save_session:None,
-        persist_session:None,
+        save_session: None,
+        persist_session: None,
     };
     let res = infer(&_infer);
 }
